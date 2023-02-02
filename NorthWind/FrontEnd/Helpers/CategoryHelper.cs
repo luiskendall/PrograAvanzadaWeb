c525878d713da2d92d4daf0ddc38393ab93783fa
luiskendall <luiske83@outlook.es>
2023-02-02 12:57:30 -0600

    GetAll y Get de Category FrontEnd

---
+++ NorthWind/FrontEnd/Helpers/CategoryHelper.cs
@@ -0,0 +1,43 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+
+namespace FrontEnd.Helpers
+{
+    public class CategoryHelper
+    {
+        private ServiceRepository ServiceRepository;
+
+        public CategoryHelper()
+        {
+            ServiceRepository = new ServiceRepository();
+        }
+        public List<CategoryViewModel> GetAll()
+        {
+
+            List<CategoryViewModel> lista;
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/category/");
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            lista = JsonConvert.DeserializeObject<List<CategoryViewModel>>(content);
+
+            return lista;
+        }
+
+        public CategoryViewModel Get(int id)
+        {
+
+            CategoryViewModel category;
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/category/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+            return category;
+        }
+
+    }
+}
