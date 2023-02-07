e3b43ab54db84c037fd95fb4a43b158c94d8c736
luiskendall <luiske83@outlook.es>
2023-02-07 14:04:14 -0600

    CRUD FrontEnd Shipper listo

---
+++ NorthWind/FrontEnd/Controllers/ShipperController.cs
@@ -0,0 +1,112 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class ShipperController : Controller
+    {
+
+        ShipperHelper shipperHelper;
+
+        // GET: ShipperController
+        public ActionResult Index()
+        {
+            shipperHelper = new ShipperHelper();
+            List<ShipperViewModel> list = shipperHelper.GetAll();
+            return View(list);
+        }
+
+        // GET: ShipperController/Details/5
+        public ActionResult Details(int id)
+        {
+
+            shipperHelper = new ShipperHelper();
+
+            ShipperViewModel shipper = shipperHelper.Get(id);
+
+            return View(shipper);
+        }
+
+        // GET: ShipperController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShipperController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ShipperViewModel ship)
+        {
+            try
+            {
+                shipperHelper = new ShipperHelper();
+
+                ShipperViewModel shipper = shipperHelper.Create(ship);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShipperController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            shipperHelper = new ShipperHelper();
+
+            ShipperViewModel shipper = shipperHelper.Get(id);
+
+            return View(shipper);
+        }
+
+        // POST: ShipperController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ShipperViewModel ship)
+        {
+            try
+            {
+                shipperHelper = new ShipperHelper();
+
+                ShipperViewModel shipper = shipperHelper.Edit(ship);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ShipperController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            shipperHelper = new ShipperHelper();
+
+            ShipperViewModel shipper = shipperHelper.Get(id);
+            return View(shipper);
+        }
+
+        // POST: ShipperController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                shipperHelper = new ShipperHelper();
+
+                ShipperViewModel shipper = shipperHelper.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NorthWind/FrontEnd/Helpers/ShipperHelper.cs
@@ -0,0 +1,79 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class ShipperHelper
+    {
+        private ServiceRepository ServiceRepository;
+
+        public ShipperHelper()
+        {
+            ServiceRepository = new ServiceRepository();
+        }
+        public List<ShipperViewModel> GetAll()
+        {
+
+            List<ShipperViewModel> lista;
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/shipper/");
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            lista = JsonConvert.DeserializeObject<List<ShipperViewModel>>(content);
+
+            return lista;
+        }
+
+        public ShipperViewModel Get(int id)
+        {
+
+            ShipperViewModel shipper;
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/shipper/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+
+            return shipper;
+        }
+
+        public ShipperViewModel Create(ShipperViewModel ship)
+        {
+
+            ShipperViewModel shipper;
+
+            HttpResponseMessage responseMessage = ServiceRepository.PostResponse("api/shipper/", ship);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+
+            return shipper;
+        }
+
+        public ShipperViewModel Delete(int id)
+        {
+
+            ShipperViewModel shipper;
+
+            HttpResponseMessage responseMessage = ServiceRepository.DeleteResponse("api/shipper/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+
+            return shipper;
+        }
+
+        public ShipperViewModel Edit(ShipperViewModel ship)
+        {
+
+            ShipperViewModel shipper;
+
+            HttpResponseMessage responseMessage = ServiceRepository.PutResponse("api/shipper/", ship);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+
+            shipper = JsonConvert.DeserializeObject<ShipperViewModel>(content);
+
+            return shipper;
+        }
+    }
+}
