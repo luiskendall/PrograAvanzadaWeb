8a2debc01691b5b0d3ded40c7a056caa1495473f
luiskendall <lsegura70860@ufide.ac.cr>
2023-03-15 00:17:19 -0600

    ApiKey implementado en BackEnd

--- NorthWind/BackEnd/Program.cs
+++ NorthWind/BackEnd/Program.cs
@@ -2,6 +2,7 @@ using Entities.Utilities;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
+using BackEnd.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +35,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.UseAuthorization();
 
